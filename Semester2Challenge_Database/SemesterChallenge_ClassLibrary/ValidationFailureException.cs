ecfef8ba68d06cf6d06165275403ed5405277271
Taylor Anderson <tjandesn@gmail.com>
2018-06-29 16:39:45 +1000

    Distinction Reached.

---
+++ Semester2Challenge_Database/SemesterChallenge_ClassLibrary/ValidationFailureException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemesterChallenge_ClassLibrary
+{
+    public class ValidationFailureException: Exception
+    {
+        public ValidationFailureException(string message) : base("ValidationFailureException: " + message)
+        {
+
+        }
+    }
+}
--- Semester2Challenge_Database/SemesterChallenge_WPF/MainWindow.xaml.cs
+++ Semester2Challenge_Database/SemesterChallenge_WPF/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -26,12 +27,17 @@ namespace SemesterChallenge_WPF
         public List<Procedure> AllProcedures { set; get; }
         public List<Treatment> AllTreatments { set; get; }
         public List<Treatment> FilteredTreatments { set; get; }
+        public List<Owner> AllOwners { set; get; }
+
+        public NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
 
         public MainWindow()
         {
             InitializeComponent();
 
             FillProcedures();
+
+            FillOwners();
         }
 
         public async Task FillProcedures()
@@ -50,6 +56,24 @@ namespace SemesterChallenge_WPF
             AllProcedures = JsonConvert.DeserializeObject<List<Procedure>>(procedure);
 
             dgProcedures.ItemsSource = AllProcedures;
+        }
+
+        public async Task FillOwners()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://semesterchallenge2.azurewebsites.net/api/OwnerViews");
+
+            string owner = "";
+
+            HttpResponseMessage response = await client.GetAsync(client.BaseAddress);
+            if (response.IsSuccessStatusCode)
+            {
+                owner = await response.Content.ReadAsStringAsync();
+            }
+
+            AllOwners = JsonConvert.DeserializeObject<List<Owner>>(owner);
+
+            dgOwners.ItemsSource = AllOwners;
         }
 
         private void dgProcedures_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -84,6 +108,169 @@ namespace SemesterChallenge_WPF
             }
 
             dgTreatments.ItemsSource = FilteredTreatments;
+        }
+
+        private void dgOwners_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Owner selectedOwner = (Owner)dgOwners.SelectedItem;
+
+            int ownID = selectedOwner.OwnerID;
+            string firstN = selectedOwner.GivenName;
+            string lastN = selectedOwner.Surname;
+            string phone = selectedOwner.Phone;
+
+            txtID.Text = Convert.ToString(ownID);
+            txtFirst.Text = firstN;
+            txtLast.Text = lastN;
+            txtPhone.Text = phone;
+        }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var regex1 = new Regex("^[0-9]+$");
+                var regex2 = new Regex("^[a-zA-Z]+$");
+                if (!regex1.IsMatch(txtID.Text))
+                {
+                    throw new ValidationFailureException("Validation Failed!");
+                }
+                else if (!regex2.IsMatch(txtFirst.Text))
+                {
+                    throw new ValidationFailureException("Validation Failed!");
+                }
+                else if (!regex2.IsMatch(txtLast.Text))
+                {
+                    throw new ValidationFailureException("Validation Failed!");
+                }
+                else if (!regex1.IsMatch(txtPhone.Text))
+                {
+                    throw new ValidationFailureException("Validation Failed!");
+                }
+                else
+                {
+                    Owner newOwner = new Owner();
+
+                    newOwner.OwnerID = Convert.ToInt32(txtID.Text);
+                    newOwner.GivenName = txtFirst.Text;
+                    newOwner.Surname = txtLast.Text;
+                    newOwner.Phone = txtPhone.Text;
+
+                    CreateOwner(newOwner);
+                }
+            }
+            catch (ValidationFailureException ex)
+            {
+                MessageBox.Show("Validation Failed!");
+                logger.Debug("ValidationFailureException");
+            }
+        }
+
+        public async Task CreateOwner(Owner newOwner)
+        {
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri("http://semesterchallenge2.azurewebsites.net/api/OwnerViews");
+
+            string owner = JsonConvert.SerializeObject(newOwner, Formatting.None);
+
+            var content = new StringContent(owner, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await client.PostAsync("owners", content);
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var regex1 = new Regex("^[0-9]+$");
+                var regex2 = new Regex("^[a-zA-Z]+$");
+                if (!regex1.IsMatch(txtID.Text))
+                {
+                    throw new ValidationFailureException("Validation Failed!");
+                }
+                else if (!regex2.IsMatch(txtFirst.Text))
+                {
+                    throw new ValidationFailureException("Validation Failed!");
+                }
+                else if (!regex2.IsMatch(txtLast.Text))
+                {
+                    throw new ValidationFailureException("Validation Failed!");
+                }
+                else if (!regex1.IsMatch(txtPhone.Text))
+                {
+                    throw new ValidationFailureException("Validation Failed!");
+                }
+                else
+                {
+                    Owner updateOwner = new Owner();
+
+                    updateOwner.OwnerID = Convert.ToInt32(txtID.Text);
+                    updateOwner.GivenName = txtFirst.Text;
+                    updateOwner.Surname = txtLast.Text;
+                    updateOwner.Phone = txtPhone.Text;
+
+                    UpdateOwner(updateOwner);
+                }
+            }
+            catch (ValidationFailureException ex)
+            {
+                MessageBox.Show("Validation Failed!");
+                logger.Debug("ValidationFailureException");
+            }
+        }
+
+        public async Task UpdateOwner(Owner updateOwner)
+        {
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri("http://semesterchallenge2.azurewebsites.net/api/");
+
+            string owner = JsonConvert.SerializeObject(updateOwner, Formatting.None);
+
+            var content = new StringContent(owner, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await client.PutAsync("owners/" + updateOwner.OwnerID, content);
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            Owner deleteOwner = new Owner();
+
+            deleteOwner.OwnerID = Convert.ToInt32(txtID.Text);
+            deleteOwner.GivenName = txtFirst.Text;
+            deleteOwner.Surname = txtLast.Text;
+            deleteOwner.Phone = txtPhone.Text;
+
+            DeleteOwner(deleteOwner);
+        }
+
+        public async Task DeleteOwner(Owner deleteOwner)
+        {
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri("http://semesterchallenge2.azurewebsites.net/api/");
+
+            string owner = JsonConvert.SerializeObject(deleteOwner, Formatting.None);
+
+            var content = new StringContent(owner, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await client.DeleteAsync("owners/" + deleteOwner.OwnerID);
+        }
+
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            FillOwners();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            logger.Trace("Application Launched");
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            logger.Trace("Application Exited");
         }
     }
 }
