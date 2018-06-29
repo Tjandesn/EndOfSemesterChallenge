fcfa6adaee58438205f0556805609710f57682ec
Taylor Anderson <tjandesn@gmail.com>
2018-06-29 14:20:19 +1000

    Pass Mark Achieved

--- Semester2Challenge_Database/SemesterChallenge_WPF/MainWindow.xaml.cs
+++ Semester2Challenge_Database/SemesterChallenge_WPF/MainWindow.xaml.cs
@@ -24,6 +24,8 @@ namespace SemesterChallenge_WPF
     public partial class MainWindow : Window
     {
         public List<Procedure> AllProcedures { set; get; }
+        public List<Treatment> AllTreatments { set; get; }
+        public List<Treatment> FilteredTreatments { set; get; }
 
         public MainWindow()
         {
@@ -48,6 +50,40 @@ namespace SemesterChallenge_WPF
             AllProcedures = JsonConvert.DeserializeObject<List<Procedure>>(procedure);
 
             dgProcedures.ItemsSource = AllProcedures;
+        }
+
+        private void dgProcedures_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Procedure selectedProcedure = (Procedure)dgProcedures.SelectedItem;
+
+            int procID = selectedProcedure.ProcedureID;
+
+            FillTreatments(procID);
+        }
+
+        public async Task FillTreatments( int procID)
+        {
+            FilteredTreatments = new List<Treatment>();
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://semesterchallenge2.azurewebsites.net/api/TreatmentViews");
+
+            string treatment = "";
+
+            HttpResponseMessage response = await client.GetAsync(client.BaseAddress);
+            if (response.IsSuccessStatusCode)
+            {
+                treatment = await response.Content.ReadAsStringAsync();
+            }
+
+            AllTreatments = JsonConvert.DeserializeObject<List<Treatment>>(treatment);
+
+            var filtered = AllTreatments.Where(tr => tr.ProcedureID == procID);
+            foreach (var item in filtered)
+            {
+                FilteredTreatments.Add(item);
+            }
+
+            dgTreatments.ItemsSource = FilteredTreatments;
         }
     }
 }
